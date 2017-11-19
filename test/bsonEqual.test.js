ab82fde72ab0625cf7c6dc8c70413a1382d1c857
Durran Jordan <durran@gmail.com>
2017-11-19 12:05:14 +0100

    Initial commit with test shell

---
+++ test/bsonEqual.test.js
@@ -0,0 +1,35 @@
+describe('#bsonEqual', () => {
+  context('when the value is an object id', () => {
+    context('when the values are equal', () => {
+      it('returns true', () => {
+
+      });
+    });
+
+    context('when the values are not equal', () => {
+      it('returns false', () => {
+
+      });
+    });
+  });
+
+  context('when the value is a decimal 128', () => {
+
+  });
+
+  context('when the value is a long', () => {
+
+  });
+
+  context('when the value is an int32', () => {
+
+  });
+
+  context('when the value is a double', () => {
+
+  });
+
+  context('when the value is a string', () => {
+
+  });
+});
